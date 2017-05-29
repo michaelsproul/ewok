0c387576e0267826449fe194b91f98675d9b940f
Andreas Fackler <andreas.fackler@maidsafe.net>
2017-05-29 17:40:18 +0200

    Fix sibling calculation in merge logic.

--- src/merge.rs
+++ src/merge.rs
@@ -22,14 +22,14 @@ pub fn merge_blocks(current_blocks: &CurrentBlocks,
                 };
                 votes.insert(vote);
             }
-        } else {
+        } else if let Some(candidate_sibling_pfx) = candidate.prefix.sibling() {
             // The block doesn't contain our name - it might be our sibling or a sibling of our
             // ancestor (for each our block it might be different). If that is the case, we vote
             // for merging from our block with every sibling of that block.
             for block in our_blocks(current_blocks, our_name) {
-                if let Some(sibling_prefix) = block.prefix.sibling() {
-                    if sibling_prefix.is_prefix_of(&block.prefix) {
-                        for sibling_block in blocks_for_prefix(current_blocks, sibling_prefix) {
+                if candidate_sibling_pfx.is_prefix_of(&block.prefix) {
+                    if let Some(block_sibling) = block.prefix.sibling() {
+                        for sibling_block in blocks_for_prefix(current_blocks, block_sibling) {
                             let target = merged_block(sibling_block, block);
                             let vote = Vote {
                                 from: block.clone(),

85b48e79d82a5ceeeb20dd82b12dc93e472e5e8c
Michael Sproul <micsproul@gmail.com>
2017-06-02 18:50:13 +1000

    Timeout after 3 mins in run.py and don't capture stdout.

--- run.py
+++ run.py
@@ -4,16 +4,18 @@ import sys
 import subprocess as sp
 
 program = ["target/release/ewok"]
+timeout = 180
 
 def main():
     i = 0
     while True:
         print("run {}".format(i))
         try:
-            sp.run(program, stdout=sp.PIPE, check=True)
+            sp.run(program, check=True, timeout=timeout)
         except sp.CalledProcessError as e:
-            print(e.output)
             sys.exit(1)
+        except sp.TimeoutExpired as e:
+            print("timed out after {} seconds".format(e.timeout))
         i += 1
 
 if __name__ == "__main__":
